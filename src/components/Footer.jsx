731efafb058144e4af4093b3d990ba4ec8dfa2bb
Alexander Rankov <alexanderrankov@gmail.com>
2025-10-16 09:24:30 -0400

    I added a few file component for the header and I added some components into the main app and they appear!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./Footer.scss";
 
 
-function Footer()
+export default function Footer()
 {
     return(
         <footer>
-
+            <div className="elements">
+                <ul>
+                    <li>711, Route Harwood, Suite 100, Vaudreuil-Dorion, Québec, J7V 8P2</li>
+                    <li>514-425-3535</li>
+                    <li>info@kellylasolution.ca</li>
+                    <li>RBQ 5773-3701-01</li>
+                    <li>® 2003 Kelly La Solution Inc. Tous droits réservés</li>
+                </ul>
+            </div>
         </footer>
     )
 }
-
-export default Footer;
