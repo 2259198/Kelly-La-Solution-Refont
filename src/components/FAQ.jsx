bc648a74c84049013a940e441cecb295be4e5800
Alexander Rankov <alexanderrankov@gmail.com>
2026-01-25 15:13:50 -0500

    I translated all of the titles of the FAQ and reorganized the footer. I'm going to translate most of the other stuff in the other files

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./FAQ.scss";
 import faq from "./Data/FAQ";
+import { useTranslation } from "react-i18next";
 
 export default function FAQ() {
+
+    const {t} = useTranslation();
+
     return (
         <div className="FAQ">
             <h2>English below</h2>
@@ -10,9 +14,9 @@ export default function FAQ() {
             <div className="questions-wrapper">
                 {faq.map((question, index) => (
                     <div className="questions" key={index}>
-                        <h4>Question</h4>
+                        <h4>{t("questions.title-question")}</h4>
                         <p>{question.question}</p>
-                        <h4>Réponse</h4>
+                        <h4>{t("questions.title-answer")}</h4>
                         <p>{question.answer}</p>
                     </div>
                 ))}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./Footer.scss";
 
 
-export default function Footer()
-{
-    return(
+export default function Footer() {
+    return (
         <footer className="footer">
             <div className="elements">
                 <ul>
-                    <li>711, Route Harwood, Suite 100, Vaudreuil-Dorion, Québec, J7V 8P2</li>
-                    <li>514-425-3535</li>
-                    <li>info@kellylasolution.ca</li>
-                    <li>RBQ 5773-3701-01</li>
+                    <div className="company-information">
+                        {/* Fix the SCSS in the future */}
+                        <li>711, Route Harwood, Suite 100, Vaudreuil-Dorion, Québec, J7V 8P2</li>
+                        <li>514-425-3535</li>
+                        <li>info@kellylasolution.ca</li>
+                        <li>RBQ 5773-3701-01</li>
+                    </div>
+
                     <li>® {new Date().getFullYear()} Kelly La Solution Inc. Tous droits réservés</li>
                 </ul>
             </div>
