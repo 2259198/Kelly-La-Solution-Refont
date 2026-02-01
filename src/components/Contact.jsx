8c1d71215caa9005984e5c63cebf019d84170f22
Alexander Rankov <alexanderrankov@gmail.com>
2026-02-01 17:42:14 -0500

    I'm continuing with the translation this time for the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,46 +1,51 @@
-import React, { useTransition } from "react";
+import React from "react";
 import "./Contact.scss";
 import members from "./Data/Members";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faPhone, faEnvelope, faE } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 
-
 export default function Contact() {
+  const { t } = useTranslation();
 
-    const {t} = useTranslation();
+  return (
+    <div className="Contact">
+      <div className="contact-container">
+        <h1>{t("contact.title")}</h1>
 
-    return (
-        <div className="Contact">
-            <div className="contact-container">
-                <h1>{t("contact.title")}</h1>
-                {/* <h3>Nous offrons nos services partout dans la province du Québec</h3> */}
-                <div className="company-info">
-                    <p> <FontAwesomeIcon icon={faHouse} /> 711 Route Harwood Suite 110 Vaudreuil-Dorion (Québec) J7V 8P2</p>
-                    <p> <FontAwesomeIcon icon={faPhone} /> 514 425-3535 | Sans frais 1 877 858-3535 | 418 842-3636</p>
-                    <p> <FontAwesomeIcon icon={faEnvelope} /> info@kellylasolution.ca</p>
-                </div>
-
-            </div>
-
-            <div className="members-section">
-                <h2> {t("contact.members")}</h2>
-                {
-                    members.map((member) => {
-                        return (
-                            <div className="members">
-                                <img src={member.image} alt={member.imageDescription} />
-                                <h4>{member.name}</h4>
-                                <p>{member.role}</p>
-                                <a href="mark@kellylasolution.ca">{member.email}</a>
-
-                            </div>
-                        );
-                    })
-                }
-            </div>
-
+        <div className="company-info">
+          <p>
+            <FontAwesomeIcon icon={faHouse} />{" "}
+            711 Route Harwood Suite 110 Vaudreuil-Dorion (Québec) J7V 8P2
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faPhone} /> 514 425-3535 | Sans frais 1 877
+            858-3535 | 418 842-3636
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} /> info@kellylasolution.ca
+          </p>
         </div>
-    )
+      </div>
+
+      <div className="members-section">
+        <h2>{t("contact.members-title")}</h2>
+
+        {members.map((member, index) => (
+          <div className="members" key={index}>
+            <img
+              src={member.image}
+              alt={t(member.imageDescription)}
+            />
+            <h4>{t(member.name)}</h4>
+            <p>{t(member.role)}</p>
+            <a href={`mailto:${t(member.email)}`}>
+              {t(member.email)}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/Data/Members.js
+++ src/components/Data/Members.js
@@ -1,22 +1,21 @@
 import MarkKelly from "../Photos/Mark_Kelly.jpg";
 import GenevieveKelly from "../Photos/Genevieve_Kelly.jpg";
 
-
 const members = [
-    {
-        image: MarkKelly,
-        imageDescription: "This is an image of Mark Kelly",
-        name: "Mark Kelly",
-        role:"Président",
-        email:"mark@kellylasolution.ca",
-    },
-    {
-        image: GenevieveKelly,
-        imageDescription: "This is an image of Geneviève Kelly",
-        name: "Geneviève Kelly",
-        role:"Adjointe administrative",
-        email:"genevieve@kellylasolution.ca",
-    }
-]
+  {
+    image: MarkKelly,
+    imageDescription: "contact.members.imageDescription",
+    name: "contact.members.name",
+    role: "contact.members.role",
+    email: "contact.members.email",
+  },
+  {
+    image: GenevieveKelly,
+    imageDescription: "contact.members.genevieve.imageDescription",
+    name: "contact.members.genevieve.name",
+    role: "contact.members.genevieve.role",
+    email: "contact.members.genevieve.email",
+  },
+];
 
 export default members;
