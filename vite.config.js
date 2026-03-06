0dc63041900ff6646476bd8e7e80f285384cfe73
Alexander Rankov <alexanderrankov@gmail.com>
2026-03-06 15:07:43 -0500

    error fixes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Kelly-La-Solution-Refont/',
   plugins: [
     react({
-      base: '/Kelly-La-Solution-Refont/',
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
